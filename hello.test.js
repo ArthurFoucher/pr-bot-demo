306ebc40dddeb5da72fcdb5c79d22e3005f1c86b
Arthur Foucher <foucherarthur@gmail.com>
2018-01-18 16:27:13 -0500

    Added stuff to trigger travis

--- hello.test.js
+++ hello.test.js
@@ -4,6 +4,7 @@ var test = require("tape");
 var hello = require("./hello.js");
 
 test("hello's output", function(t) {
+  t.equal(hello("Alice"), "Hello Alice");
   t.equal(hello("Bob"), "Hello Bob");
   t.end();
 });
