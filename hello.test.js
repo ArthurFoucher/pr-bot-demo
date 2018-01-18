fc73b4a57b9c58a43abf34ab4eeaa5e920192a4e
Arthur Foucher <foucherarthur@gmail.com>
2018-01-18 15:08:36 -0500

    Added method

--- hello.test.js
+++ hello.test.js
@@ -1,9 +1,16 @@
-'use strict';
+"use strict";
 
-var test = require('tape');
-var hello = require('./hello.js');
+var test = require("tape");
+var funcs = require("./hello.js");
+var hello = funcs.hello;
+var goodbye = funcs.goodbye;
 
-test('hello\'s output', function (t) {
-  t.equal(hello(), 'Hello World');
+test("hello's output", function(t) {
+  t.equal(hello(), "Hello World");
+  t.end();
+});
+
+test("goodbye's output", function(t) {
+  t.equal(goodbye(), "Goodbye World");
   t.end();
 });
